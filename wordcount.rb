02ef4f8c3136cb8cae4be1df7fb57f62f04bd814
Femi Senjobi <femisenjobi@gmail.com>
2015-05-12 11:21:11 +0100

    This is the first version of the word count

---
+++ wordcount.rb
@@ -0,0 +1,11 @@
+def wordcount(text)
+	frequencies = Hash.new(0)
+	words = text.split(" ")
+	words.each do |word|
+		frequencies[word] += 1
+	end
+	frequencies.each {|x,y| puts "#{x} came up #{y} times}"}
+end
+puts "Enter your words"
+user_input = gets.chomp
+puts wordcount(user_input)
